5a404e5f073d9e942a6f3f7c75553a9932e58ec8
Alpccl <aledleonv@gmail.com>
2021-01-17 18:27:02 -0300

    Actualizacion Con Sistema de Navegacion

--- app/src/main/java/com/example/triviaapp/TriviaFragment.java
+++ app/src/main/java/com/example/triviaapp/TriviaFragment.java
@@ -2,63 +2,92 @@ package com.example.triviaapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TriviaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.triviaapp.databinding.FragmentTriviaBinding;
+
+import java.util.zip.Inflater;
+
+
 public class TriviaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+private FragmentTriviaBinding mBinding;
 
     public TriviaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TriviaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TriviaFragment newInstance(String param1, String param2) {
-        TriviaFragment fragment = new TriviaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trivia, container, false);
+        mBinding = FragmentTriviaBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
     }
-}
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String saludo = getString(R.string.saludo);
+        mBinding.tvSaludo.setText(saludo);
+
+        mBinding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                int index = mBinding.radioGroup.indexOfChild(mBinding.radioGroup.findViewById(i));
+                boolean choice;
+                if (index == 2){
+                    choice = true;
+                    Navigation.findNavController(mBinding.getRoot()).navigate(R.id.action_triviaFragment_to_triviaGone);
+                } else {
+                    choice = false;
+                    Navigation.findNavController(mBinding.getRoot()).navigate(R.id.action_triviaFragment_to_triviaFail);
+                }
+            }
+        });
+    }
+            }
+
+    /*private void WinnerFragment(String name) {
+        //Instanciar el fragmento que vamos añadir
+        WinnerFragment winnerFragment = WinnerFragment.newInstance(" name");
+        //Instanciar el fragment Manager
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        //Instanciar la transaccion
+        FragmentTransaction transaction = fragmentManager.beginTransaction()
+                .replace(R.id.fragment, winnerFragment, TriviaFragment.class.getSimpleName())
+                // añadir el fragmento a la pila
+         .addToBackStack(null);
+        //Activiar la transacción
+        transaction.commit();
+    }
+
+    private void LooserFragment(String name){
+        //Instanciar el fragmento que vamos añadir
+        LooserFragment looserFragment = com.example.ejercicio7.LooserFragment.newInstance("name");
+        //Instanciar el fragment Manager
+        FragmentManager supportFragmentManager = getActivity().getSupportFragmentManager();
+        //Instanciar la transaccion
+        place(R.id.fragment, looserFragment, TriviaFragment.class.getSimpleName())
+                // añadir el fragmento a la pila
+                .addToBackStack(null);
+        //Activiar la transacción
+        transaction1.commit();
+    }
+}*/
--- app/src/main/java/com/example/triviaapp/TriviaGone.java
+++ app/src/main/java/com/example/triviaapp/TriviaGone.java
@@ -3,6 +3,7 @@ package com.example.triviaapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,11 +20,6 @@ public class TriviaGone extends Fragment {
 
     private FragmentTriviaGoneBinding mbinding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -32,37 +28,25 @@ public class TriviaGone extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TriviaGone.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TriviaGone newInstance(String param1, String param2) {
-        TriviaGone fragment = new TriviaGone();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trivia_gone, container, false);
+      mbinding = FragmentTriviaGoneBinding.inflate(inflater, container, false);
+        return mbinding.getRoot();
+
+        mbinding.btcomenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(mbinding.getRoot()).navigate(R.id.action_triviaGone_to_triviaFragment);
+            }
+        });
     }
 }
